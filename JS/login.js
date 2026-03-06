558f4c2d9999b58f2bcff9d02f92f663d205ddf5
Nazmul-vs-code <khalekbiton1977@gmail.com>
2026-03-06 21:06:44 +0600

    first step done

---
+++ JS/login.js
@@ -0,0 +1,16 @@
+
+const signin = () => {
+    const email = document.getElementById('input-email');
+    
+    const email_value = email.value;
+    
+    const password = document.getElementById('input-password');
+    const pass_value = password.value;
+    if (email_value=='admin' && pass_value=='admin123') {
+        window.location.href = "../HTML/main.html";
+    }
+    else{
+        confirm("Invalid credentials!! Please, make sure to use actual username and password.")
+    }
+}
+
